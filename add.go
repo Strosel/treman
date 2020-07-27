256bc9f0f3d962fb23fb3bb98ac5efa069532828
axel paulander <axel.paulander@gmail.com>
2020-07-28 00:35:19 +0200

    bump gio version

--- add.go
+++ add.go
@@ -15,75 +15,65 @@ var (
 	d1Edit     = new(widget.Editor)
 	d2Edit     = new(widget.Editor)
 	nameEdit   = new(widget.Editor)
-	saveButton = new(widget.Button)
+	saveButton = new(widget.Clickable)
 )
 
-func addRule(gtx *layout.Context, th *material.Theme) {
-	radio := func() {
+func addRule(gtx Ctx, th *material.Theme) {
+	radio := func(gtx Ctx) Dim {
 		in := layout.UniformInset(unit.Dp(0))
 		in.Top = unit.Dp(64)
-		in.Layout(gtx, func() {
-			layout.Flex{
+		return in.Layout(gtx, func(gtx Ctx) Dim {
+			return layout.Flex{
 				Spacing: layout.SpaceAround,
 			}.Layout(gtx,
-				layout.Rigid(func() {
-					th.RadioButton("sum", "Summa").Layout(gtx, ruleRadio)
-				}),
-				layout.Rigid(func() {
-					th.RadioButton("set", "Par").Layout(gtx, ruleRadio)
-				}),
-				layout.Rigid(func() {
-					th.RadioButton("single", "En tärning").Layout(gtx, ruleRadio)
-				}),
+				layout.Rigid(material.RadioButton(th, ruleRadio, "sum", "Summa").Layout),
+				layout.Rigid(material.RadioButton(th, ruleRadio, "set", "Par").Layout),
+				layout.Rigid(material.RadioButton(th, ruleRadio, "single", "En tärning").Layout),
 			)
 		})
 	}
 
-	rolls := func() {
-		edit := th.Editor("0")
-		edit.TextSize = fontSize
+	rolls := func(gtx Ctx) Dim {
+		// edit.TextSize = fontSize
 		in := layout.UniformInset(unit.Dp(50))
-		layout.Flex{
+		return layout.Flex{
 			Alignment: layout.Middle,
 			Spacing:   layout.SpaceSides,
 		}.Layout(gtx,
-			layout.Rigid(func() {
-				in.Layout(gtx, func() {
-					edit.Layout(gtx, d1Edit)
-				})
+			layout.Rigid(func(gtx Ctx) Dim {
+				return in.Layout(gtx, material.Editor(th, d1Edit, "0").Layout)
 			}),
-			layout.Rigid(func() {
-				if ruleRadio.Value(gtx) == "set" {
-					in.Layout(gtx, func() {
-						edit.Layout(gtx, d2Edit)
-					})
+			layout.Rigid(func(gtx Ctx) Dim {
+				if ruleRadio.Value == "set" {
+					return in.Layout(gtx, material.Editor(th, d2Edit, "0").Layout)
 				}
+				return Dim{}
 			}),
 		)
 	}
 
-	text := func() {
-		edit := th.Editor("Regel")
+	text := func(gtx Ctx) Dim {
+		edit := material.Editor(th, nameEdit, "Regel")
 		edit.TextSize = bigFont
-		edit.Layout(gtx, nameEdit)
+		return edit.Layout(gtx)
 	}
 
-	save := func() {
-		bttn := th.Button("\nSpara\n")
+	save := func(gtx Ctx) Dim {
+		bttn := material.Button(th, saveButton, "\nSpara\n")
 		bttn.TextSize = fontSize
-		for saveButton.Clicked(gtx) {
-			if saveRule(gtx) == nil {
+		for saveButton.Clicked() {
+			if saveRule() == nil {
 				playing = true
 				d1Edit.SetText("")
 				d2Edit.SetText("")
 				nameEdit.SetText("")
 			}
 		}
-		bttn.Layout(gtx, saveButton)
+		return bttn.Layout(gtx)
 	}
 
-	layout.UniformInset(unit.Dp(8)).Layout(gtx, func() {
-		layout.Flex{
+	layout.UniformInset(unit.Dp(8)).Layout(gtx, func(gtx Ctx) Dim {
+		return layout.Flex{
 			Axis:      layout.Vertical,
 			Alignment: layout.End,
 		}.Layout(gtx,
@@ -95,28 +85,28 @@ func addRule(gtx *layout.Context, th *material.Theme) {
 	})
 }
 
-func saveRule(gtx *layout.Context) error {
+func saveRule() error {
 	d1, err := strconv.Atoi(d1Edit.Text())
 	if err != nil {
 		return err
 	}
 	d2, err := strconv.Atoi(d2Edit.Text())
-	if err != nil && ruleRadio.Value(gtx) == "set" {
+	if err != nil && ruleRadio.Value == "set" {
 		return err
 	}
 
 	var nrule Rule
-	if ruleRadio.Value(gtx) == "sum" && d1 > 1 {
+	if ruleRadio.Value == "sum" && d1 > 1 {
 		nrule = SumRule{
 			Name: nameEdit.Text(),
 			Sum:  d1,
 		}
-	} else if ruleRadio.Value(gtx) == "set" && d1 > 0 && d1 < 7 && d2 > 0 && d2 < 7 {
+	} else if ruleRadio.Value == "set" && d1 > 0 && d1 < 7 && d2 > 0 && d2 < 7 {
 		nrule = SetRule{
 			Name: nameEdit.Text(),
 			Set:  Roll{d1, d2},
 		}
-	} else if ruleRadio.Value(gtx) == "single" && d1 > 0 && d1 < 7 {
+	} else if ruleRadio.Value == "single" && d1 > 0 && d1 < 7 {
 		nrule = SingleRule{
 			Name: nameEdit.Text(),
 			Dice: d1,
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/strosel/treman
 go 1.13
 
 require (
-	gioui.org v0.0.0-20200211144101-e8add40440b3
+	gioui.org v0.0.0-20200726090130-3b95e2918359
 	github.com/markbates/pkger v0.14.0
-	golang.org/x/image v0.0.0-20190802002840-cff245a6509b
+	golang.org/x/image v0.0.0-20200618115811-c13761719519
 )
