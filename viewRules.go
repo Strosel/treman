a78d815066bcdefe7bdb67e6922c0312ccb69327
axel paulander <axel.paulander@gmail.com>
2020-09-03 13:40:59 +0200

    Removed GOOS string from rules

--- viewRules.go
+++ viewRules.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"image/color"
+	"runtime"
+
 	"gioui.org/layout"
 	"gioui.org/unit"
+	"gioui.org/widget"
 	"gioui.org/widget/material"
+	"golang.org/x/image/colornames"
 )
 
 var baserules = `Först väljs en spelare till treman, detta är en titel som kommer förflytta sig under spelets gång. 
@@ -21,7 +26,8 @@ n+n klunkar betyder att n st klunkar får delas ut till två personer eller dubb
 type viewRules struct {
 	rules []Rule
 
-	list *layout.List
+	list        *layout.List
+	cancelClick *widget.Clickable
 }
 
 func viewRulesScreen(rules []Rule) Screen {
@@ -30,6 +36,7 @@ func viewRulesScreen(rules []Rule) Screen {
 		list: &layout.List{
 			Axis: layout.Vertical,
 		},
+		cancelClick: new(widget.Clickable),
 	}
 }
 
@@ -39,8 +46,24 @@ func (v *viewRules) Layout(gtx Ctx, th *material.Theme) (nextScreen Screen) {
 	layout.UniformInset(unit.Dp(8)).Layout(gtx, func(gtx Ctx) Dim {
 		return v.list.Layout(gtx, len(v.rules)+2, func(gtx Ctx, i int) Dim {
 			return layout.UniformInset(unit.Dp(16)).Layout(gtx, func(gtx Ctx) Dim {
-				if i == 0 {
+				if i == 0 && runtime.GOOS == "android" {
 					return layout.Inset{Top: unit.Dp(16)}.Layout(gtx, material.H6(th, "Regler").Layout)
+				} else if i == 0 {
+					bttn := material.Button(th, v.cancelClick, "← Regler")
+					bttn.Color = colornames.Black
+					bttn.Background = color.RGBA{255, 255, 255, 255}
+					bttn.TextSize = material.H6(th, "").TextSize
+					bttn.Inset.Left = unit.Dp(0)
+
+					for v.cancelClick.Clicked() {
+						nextScreen = gameScreen(v.rules)
+					}
+
+					return layout.Flex{
+						Alignment: layout.Start,
+					}.Layout(gtx,
+						RigidInset(layout.Inset{Top: unit.Dp(16)}, bttn.Layout),
+					)
 				} else if i == 1 {
 					body := material.Body1(th, baserules)
 					body.TextSize = unit.Dp(24)
